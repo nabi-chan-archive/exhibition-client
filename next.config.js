aa98d0d22a6b0fffb070338c120c77dfad15425f
Pinot. Kim <pinot.kim@kakao.com>
2020-09-04 04:46:48 +0900

    :wrench: nextConfig와 jsConfig 파일을 변경하라

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const isBuild = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+    localIdentName: isBuild ? '[hash:base64:5]' : '[local]-[hash:base64:5]',
+  },
+  env: {
+  },
+};
