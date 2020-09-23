442686620b3111f1a8844b8ddb5f93bb6e0c8665
Pinot. Kim <pinot.kim@kakao.com>
2020-09-23 17:28:35 +0900

    :sparkles: Artwork 클릭시 해당 세부 페이지로 이동하는 기능을 추가하라

--- src/views/components/Artwork/Artwork.js
+++ src/views/components/Artwork/Artwork.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Link from "next/link";
 import css from './artwork.module.scss';
 
-export function Artwork({ img_src : artworkLink, title }) {
+export function Artwork({ img_src : artworkLink, title, id }) {
   return (
-      <article className={css.artwork}>
-        <img src={artworkLink} alt={title} className={css.poster} />
-      </article>
+      <Link href={`/artwork/${id}`}>
+        <a>
+          <article className={css.artwork}>
+            <img src={artworkLink} alt={title} className={css.poster} />
+          </article>
+        </a>
+      </Link>
   )
 }
