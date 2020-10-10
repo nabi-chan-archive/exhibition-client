d52fdd58b584df32c47057abdcbb8efe80829026
Pinot. Kim <pinot.kim@kakao.com>
2020-10-10 19:40:30 +0900

    :truck: pages에 getInitalProps를 추가하라

--- src/pages/artwork/index.js
+++ src/pages/artwork/index.js
@@ -4,7 +4,7 @@ import { Artworks } from "@components/Artworks/Artworks";
 import {Strip} from "@components/Strip/Strip";
 import Head from "next/head";
 
-export default function ArtworksPage() {
+export default function ArtworksPage({artworks}) {
   return (
       <>
         <Head>
@@ -16,10 +16,95 @@ export default function ArtworksPage() {
             <Menu />
           </Header>
           
-          <Artworks />
+          <Artworks artwork={artworks} />
         </main>
   
         <Strip text={'Swipe Down'} content={4} />
       </>
   )
+}
+
+ArtworksPage.getInitialProps = async ctx => {
+  return {
+    artworks: [
+      {
+        id: 1,
+        type: 'photography',
+        img_src: 'http://placehold.it/1920x1080',
+        title: 'test'
+      },
+      {
+        id: 2,
+        type: 'poster',
+        img_src: 'http://placehold.it/300x400',
+        title: 'test'
+      },
+      {
+        id: 3,
+        type: 'poster',
+        img_src: 'http://placehold.it/300x400',
+        title: 'test'
+      },
+      {
+        id: 4,
+        type: 'photography',
+        img_src: 'http://placehold.it/1920x1080',
+        title: 'test'
+      },
+      {
+        id: 5,
+        type: 'poster',
+        img_src: 'http://placehold.it/300x400',
+        title: 'test'
+      },
+      {
+        id: 6,
+        type: 'poster',
+        img_src: 'http://placehold.it/1920x1080',
+        title: 'test'
+      },
+      {
+        id: 7,
+        type: 'photography',
+        img_src: 'http://placehold.it/1920x1080',
+        title: 'test'
+      },
+      {
+        id: 8,
+        type: 'poster',
+        img_src: 'http://placehold.it/1920x1080',
+        title: 'test'
+      },
+      {
+        id: 9,
+        type: 'poster',
+        img_src: 'http://placehold.it/1920x1080',
+        title: 'test'
+      },
+      {
+        id: 10,
+        type: 'photography',
+        img_src: 'http://placehold.it/1920x1080',
+        title: 'test'
+      },
+      {
+        id: 11,
+        type: 'poster',
+        img_src: 'http://placehold.it/1920x1080',
+        title: 'test'
+      },
+      {
+        id: 12,
+        type: 'poster',
+        img_src: 'http://placehold.it/1920x1080',
+        title: 'test'
+      },
+      {
+        id: 13,
+        type: 'poster',
+        img_src: 'http://placehold.it/1920x1080',
+        title: 'test'
+      }
+    ]
+  }
 }
