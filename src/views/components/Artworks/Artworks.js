c26c3bd4c2dee08125b9a94d1d596e9ec721cf80
Pinot. Kim <pinot.kim@kakao.com>
2020-10-10 19:43:50 +0900

    :art: Artworks 컴포넌트의 구조를 변경하라
    - 4열이던 컨텐츠를 3열로 줄였습니다.
    - 반복문을 활용하여 코드의 재사용을 줄였습니다.
    - filter를 추가하여 조금 더 코드를 해석하기 편하게 설정했습니다.

--- src/views/components/Artworks/Artworks.js
+++ src/views/components/Artworks/Artworks.js
@@ -1,54 +1,8 @@
 import React from 'react';
 import css from './artworks.module.scss';
-
 import {Artwork} from "@components/Artwork/Artwork";
 
-const artworkMock = [
-  {
-    id: 1,
-    type: 'photography',
-    img_src: 'http://placehold.it/1920x1080',
-    title: 'test'
-  },
-  {
-    id: 2,
-    type: 'poster',
-    img_src: 'http://placehold.it/300x400',
-    title: 'test'
-  },
-  {
-    id: 3,
-    type: 'poster',
-    img_src: 'http://placehold.it/300x400',
-    title: 'test'
-  },
-  {
-    id: 4,
-    type: 'photography',
-    img_src: 'http://placehold.it/1920x1080',
-    title: 'test'
-  },
-  {
-    id: 5,
-    type: 'poster',
-    img_src: 'http://placehold.it/300x400',
-    title: 'test'
-  },
-  {
-    id: 6,
-    type: 'photography',
-    img_src: 'http://placehold.it/1920x1080',
-    title: 'test'
-  },
-  {
-    id: 7,
-    type: 'photography',
-    img_src: 'http://placehold.it/1920x1080',
-    title: 'test'
-  }
-]
-
-function Row({ artwork }) {
+function Row({artwork}) {
   return (
       <div className={css.row}>
         <Artwork {...artwork} />
@@ -56,23 +10,26 @@ function Row({ artwork }) {
   )
 }
 
-export function Artworks() {
-  const cell = Math.floor(artworkMock.length / 4)
+function Column({index, artwork}) {
+  const filtered = artwork.filter((_i, i) => (i - index) % 3 === 0)
   
   return (
+      <div className={css.column}>
+        {filtered.map((_i) => <Row artwork={_i} key={_i.id} />)}
+      </div>
+  )
+}
+
+export function Artworks({artwork}) {
+  return (
       <section className={css.container}>
-          <div className={css.column}>
-            {artworkMock.map((artwork, i) => i <= cell ? <Row artwork={artwork} key={i} /> : null)}
-          </div>
-          <div className={css.column}>
-            {artworkMock.map((artwork, i) => i <= cell * 2 && i > cell ? <Row artwork={artwork} key={i} /> : null)}
-          </div>
-          <div className={css.column}>
-            {artworkMock.map((artwork, i) => i <= cell * 3 && i > cell * 2 ? <Row artwork={artwork} key={i} /> : null)}
-          </div>
-          <div className={css.column}>
-            {artworkMock.map((artwork, i) => i >= cell * 3 ? <Row artwork={artwork} key={i} /> : null)}
-          </div>
+        {[0, 1, 2].map((i) => (
+            <Column
+                index={i}
+                key={i}
+                artwork={artwork}
+            />
+        ))}
       </section>
   );
 }
