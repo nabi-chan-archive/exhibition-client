cabec158e0e8254e2041ac77a5e54240e97a980b
Pinot. Kim <pinot.kim@kakao.com>
2020-11-10 22:04:05 +0900

    :bug: Header를 조금 더 시멘틱하게 만들어라

--- src/views/components/Header/Header.js
+++ src/views/components/Header/Header.js
@@ -6,13 +6,13 @@ import classcat from "classcat";
 
 export function Menu() {
   const [isOpened, setIsOpened] = useState(false);
-  
+
   const toggleOpen = () => setIsOpened(prevState => !prevState);
-  
+
   return (
       <div className={css.menu}>
         <h2 className={classcat([css.subTitle, isOpened && css.active])} onClick={toggleOpen}>category</h2>
-  
+
         <ul className={classcat([css.category, isOpened && css.active])}>
           <li>
             <Link href="/artwork"><a>all</a></Link>
@@ -30,20 +30,20 @@ export function Menu() {
 
 export function Back() {
   const router = useRouter();
-  
+
   return <h2 className={classcat([css.subTitle, css.active])} onClick={() => router.back()}>Back</h2>;
 }
 
 export function Header({ children }) {
   return (
-      <div className={css.Header}>
+      <header className={css.Header}>
         <Link href="/">
           <h1 className={css.title}>
             <a>{'20`21\n웹 아카이브전\n무균전시\n:\n새 시대\n새 빛'}</a>
           </h1>
         </Link>
-        
+
         {children}
-      </div>
+      </header>
   )
 }
