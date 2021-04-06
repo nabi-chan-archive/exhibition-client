747d16683f4bdf2c82f55de7ddf64a6fc4ae6e16
PINOT <pinot.kim@kakao.com>
2021-04-07 00:42:03 +0900

    🔧 HOST 업데이트

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,1 +1,1 @@
-export const API_PATH = process.env.NODE_ENV === "production" ? process.env.HOST : "http://localhost:3000";
+export const API_PATH = process.env.NODE_ENV === "production" ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
